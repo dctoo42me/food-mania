da375e04bc2938ed1d2e156eba9b25ff3ebffe74
David Conde <dctoo42me@gmail.com>
2024-05-08 21:04:41 -0500

    added js for horizontal scroll

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 const galleryContainer = document.querySelector('.gallery-container');
 
-// Function to handle mouse wheel scrolling
-function handleMouseWheel(event) {
-    // Adjust the scrollLeft property based on the mouse wheel delta
-    galleryContainer.scrollLeft += event.deltaY;
-    
-    // Prevent the default scrolling behavior of the page
-    event.preventDefault();
-}
+let startX = 0;
+let scrollLeft = 0;
+let isScrolling = false;
 
-// Add event listener for mouse wheel scrolling when hovering over the gallery container
-galleryContainer.addEventListener('wheel', handleMouseWheel);
+galleryContainer.addEventListener('touchstart', (e) => {
+    isScrolling = true;
+    startX = e.touches[0].clientX - galleryContainer.offsetLeft;
+    scrollLeft = galleryContainer.scrollLeft;
+});
 
-// Remove event listener for mouse wheel scrolling when not hovering over the gallery container
-galleryContainer.addEventListener('mouseleave', function() {
-    galleryContainer.removeEventListener('wheel', handleMouseWheel);
+galleryContainer.addEventListener('touchmove', (e) => {
+    if (!isScrolling) return;
+    e.preventDefault();
+    const x = e.touches[0].clientX - galleryContainer.offsetLeft;
+    const walk = (x - startX) * 2; // Adjust scrolling speed
+    galleryContainer.scrollLeft = scrollLeft - walk;
+});
+
+galleryContainer.addEventListener('touchend', () => {
+    isScrolling = false;
 });
